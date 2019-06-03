1dde34a239b41ca3324851199487f5bda71ade61
huanguxtao <h784124804@gmail.com>
2019-06-03 13:11:56 +0800

    build

---
+++ dist/precache-manifest.e6b12af4b9598169817209462b38279f.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c689b8986ab080865662",
+    "url": "/app.css"
+  },
+  {
+    "revision": "c689b8986ab080865662",
+    "url": "/app.js"
+  }
+]);
